5e996447efef79faea870734dc83c8509c747ff0
lorde <rioaformyking@gmail.com>
2022-05-12 23:24:42 +0800

    add:init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>5YouWant</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="narbar-menu" id="narbar-menu">
+        <div class="narbar-end">
+          <router-link to="/summer">Summer</router-link>
+          <router-link to="/winter">Winter</router-link>
+
+          <div class="narbar-item">
+            <div class="buttons">
+              <router-link to="/login" class="button is-light">Log in</router-link>
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fas-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '../node_modules/bulma';
+
 </style>
