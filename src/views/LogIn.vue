baa0e53e023fc0ac7cd04b7a67ab4e57f962c660
lode <rioaformyking@gmail.com>
2022-05-20 11:24:11 +0800

    add:login

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,5 +1,25 @@
 <template>
     <div class="page-login">
-        Waiting fot finish
+            <form action="" class="login">
+                <h1>Login in</h1>
+                <input placeholder="username">
+            </form>
     </div>
 </template>
+
+
+<style>
+    .login {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 2rem 3rem;
+    }
+
+    .login h1 {
+        text-align: center;
+        margin-bottom: 2rem;
+        font-size: 2rem;
+        font-weight: bolder;
+    }
+</style> 
